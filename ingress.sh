b68c4c2d7037d424d946792f30982cbf72ea55f5
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-05-22 19:03:03 +0530

    updated

--- ingress.sh
+++ ingress.sh
@@ -7,14 +7,17 @@ ACC_ID=124355635734
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
 
+#kubectl installation
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
+
+#eksctl
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+mv /tmp/eksctl /usr/local/bin
+
 ### Permissions
-
-#OIDC provider
-
-eksctl utils associate-iam-oidc-provider \
-    --region $REGION_CODE \
-    --cluster $CLUSTER_NAME \
-    --approve
 
 # Provide access to EKS through IAM Policy
 
