4a9e7a9ed87e1fbd8a5270d9e97bb34242c2c5bb
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-05-23 16:28:32 +0530

    update changes

--- ingress.sh
+++ ingress.sh
@@ -10,7 +10,7 @@ PLATFORM=$(uname -s)_$ARCH
 #kubectl installation
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mv kubectl /usr/local/bin/kubectl
+sudo mv kubectl /usr/local/bin/kubectl
 
 #eksctl
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
